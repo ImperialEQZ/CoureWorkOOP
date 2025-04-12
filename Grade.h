5d8c374df1a4ae648c2a2281a4c6ae1f6db2dbee
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-04-12 17:52:25 +0300

    Добавление сигнатуры студента и оценки за предмет

---
+++ Grade.h
@@ -0,0 +1,17 @@
+#ifndef COURSEWORKOOP_GRADE_H
+#define COURSEWORKOOP_GRADE_H
+#include <string>
+#include <iostream>
+
+class Grade {
+public:
+    std::string assignmentName;
+    int mark;
+
+    Grade(std::string name, int m) : assignmentName(name), mark(m) {}
+
+    void outputGrade() const {
+        std::cout << "Название предмета: " << assignmentName << ", Оценка: " << mark << std::endl;
+    }
+};
+#endif //COURSEWORKOOP_GRADE_H
---
+++ Student.h
@@ -0,0 +1,13 @@
+#ifndef COURSEWORKOOP_STUDENT_H
+#define COURSEWORKOOP_STUDENT_H
+#include <string>
+//Создание класса студент
+class Student {
+    std::string Name;//Имя
+    std::string SurName;//Фамилия
+    std::string MiddleName;//Отчество
+    std::string InstituteName;//Название института
+    std::string DirectionName;//Название направления
+    std::string NumberCreditBook;//Номер зачетной книжки
+};
+#endif //COURSEWORKOOP_STUDENT_H
