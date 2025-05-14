ec8036ab21eb6e7af2d818933de7bf847b3129c4
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-05-14 14:11:28 +0300

    Реализация выбора групп в разных кафедрах-институтах

--- ControlUnit.cpp
+++ ControlUnit.cpp
@@ -78,4 +78,48 @@ void ControlUnit::selectDepartment(const std::string& institute) {
 
     selectGroup(institute, department);
 }
+//Выбор групп в разных институтах и кафедрах
+void ControlUnit::selectGroup(const std::string& institute, const std::string& department) {
+    std::cout << "\nGroups:\n";
+
+    std::vector<std::string> groups;
+    if (institute == "IITUS") {
+        if (department == "POVTAS") {
+            std::cout << "1. VT-232\n2. PV-233\n";//Две группы ПОВТАСа
+            groups = {"VT-232", "PV-233"};
+        } else if (department == "IT") {
+            std::cout << "1. IST-231\n2. IST-232\n"; ////Две группы ИТ
+            groups = {"IST-231", "IST-232"};
+        } else {
+            std::cout << "No groups for this department.\n";//Не найдена группа на кафедре
+            return;
+        }
+    } else { // Институт ИЭИЭ
+        if (department == "Electricity and electrical engineering") {
+            std::cout << "1. EiE-231\n2. EiE-232\n";//Две группы ЭиЭ
+            groups = {"EK-231", "EK-232"};
+        } else if (department == "Heat power engineering and heat engineering") {
+            std::cout << "1. TiT-231\n2. TiT-232\n";//Две группы ТиТ
+            groups = {"TiT-231", "TiT-232"};
+        } else {
+            std::cout << "No groups for this department.\n";//Не найдена группа на кафедре
+            return;
+        }
+    }
+    std::cout << "3. Back\n";
+
+    int groupChoice;
+    std::cin >> groupChoice;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    if (groupChoice == 3) return;
+
+    if (groupChoice < 1 || groupChoice > groups.size()) {
+        std::cout << "Incorrect input.\n";
+        return;
+    }
+
+    std::string group = groups[groupChoice - 1];
+    selectStudent(institute, department, group);
+}
 
