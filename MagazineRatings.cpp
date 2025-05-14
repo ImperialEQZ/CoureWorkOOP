8f326b7607a03d8236659c987c6bf32910a2eebf
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-05-14 12:56:02 +0300

    Реализация getStudentInfo

--- MagazineRatings.cpp
+++ MagazineRatings.cpp
@@ -26,3 +26,24 @@ std::vector<std::string> GradeBook::getStudentIds(const std::string& institute, 
     return studentIds; // Возвращаем пустой список, если не найдено
 }
 
+json GradeBook::getStudentInfo(const std::string& institute, const std::string& department, const std::string& group, const std::string& studentId) {
+    for (auto& inst : data["institutes"]) {
+        if (inst["name"] == institute) {
+            for (auto& dep : inst["departments"]) {
+                if (dep["name"] == department) {
+                    for (auto& grp : dep["groups"]) {
+                        if (grp["name"] == group) {
+                            for (auto& student : grp["students"]) {
+                                if (student["id"] == studentId) {
+                                    return student; // Возвращаем данные о студенте
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return json{}; // Возвращаем пустой JSON-объект, если не найдено ничего
+}
+
