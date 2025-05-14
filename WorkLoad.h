9bea9d677e6610ad4a124a35df941587766cb9e0
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-05-14 20:41:08 +0300

    Исправлено: абстрактная фабрика заменена фабричным методом (что верно для моего случая)

--- WorkLoad.h
+++ WorkLoad.h
@@ -2,15 +2,27 @@
 #define COURSEWORKOOP_WORKLOAD_H
 #include <string>
 #include <memory>
+#include <stdexcept>
 //Перечисление типов учебной нагрузки
-enum class WorkloadType { LAB, EXAM, COURSE_PROJECT, CREDIT /*зачет*/ };
-//Учебная нагрузка
+enum class WorkloadType { LAB, EXAM, COURSE_PROJECT, CREDIT };
+
+// Предварительное объявление классов, для корректной работы
+class LabWork;
+class ExamWork;
+class CourseProject;
+class CreditWork;
+
+//Учебная нагрузка (базовый класс)
 class Workload {
 public:
     virtual ~Workload() = default;
     virtual WorkloadType getType() const = 0;
     virtual int getMark() const = 0;
+
+    // Паттерн - фабричный метод
+    static std::unique_ptr<Workload> createWorkload(WorkloadType type, int mark = 0);
 };
+
 //Лабораторная (имеет оценку, тип работы LAB, влияет на средний балл)
 class LabWork : public Workload {
     int mark;
@@ -41,33 +53,20 @@ public:
     WorkloadType getType() const override { return WorkloadType::CREDIT; }
     int getMark() const override { return 0; }
 };
-//Реализация Абстрактной фабрики
-class WorkloadFactory {
-public:
-    virtual ~WorkloadFactory() = default;
-    virtual std::unique_ptr<Workload> createLab(int mark) = 0;
-    virtual std::unique_ptr<Workload> createExam(int mark) = 0;
-    virtual std::unique_ptr<Workload> createCourseProject(int mark) = 0;
-    virtual std::unique_ptr<Workload> createCredit() = 0;
-};
 
-class TechnicalWorkloadFactory : public WorkloadFactory {
-public:
-    //Создание лабораторной
-    std::unique_ptr<Workload> createLab(int mark) override {
-        return std::make_unique<LabWork>(mark);
+// Реализация фабричного метода (после объявления всех классов)
+inline std::unique_ptr<Workload> Workload::createWorkload(WorkloadType type, int mark) {
+    switch (type) {
+        case WorkloadType::LAB:
+            return std::make_unique<LabWork>(mark);
+        case WorkloadType::EXAM:
+            return std::make_unique<ExamWork>(mark);
+        case WorkloadType::COURSE_PROJECT:
+            return std::make_unique<CourseProject>(mark);
+        case WorkloadType::CREDIT:
+            return std::make_unique<CreditWork>();
+        default:
+            throw std::invalid_argument("Error: Incorrect workload type");//некорректный тип рабочей нагрузки
     }
-    //Создание экзамена
-    std::unique_ptr<Workload> createExam(int mark) override {
-        return std::make_unique<ExamWork>(mark);
-    }
-    //Создание курсовой работы
-    std::unique_ptr<Workload> createCourseProject(int mark) override {
-        return std::make_unique<CourseProject>(mark);
-    }
-    //Создание зачета
-    std::unique_ptr<Workload> createCredit() override {
-        return std::make_unique<CreditWork>();
-    }
-};
+}
 #endif //COURSEWORKOOP_WORKLOAD_H
